4b125ed8d1efc1ad9137c056d16848166eee6998
nereazj544 <nzjvirtual@gmail.com>
2024-08-04 23:22:44 +0200

    s

--- java/ProyectoJava1-Anaya/src/Argumentosvariablesmetodosyoperadores/TestyEjercio2/Ejercicio_6.java
+++ java/ProyectoJava1-Anaya/src/Argumentosvariablesmetodosyoperadores/TestyEjercio2/Ejercicio_6.java
@@ -7,16 +7,15 @@ public class Ejercicio_6 {
         String nh1 = args[2]; //nombre hijo 1
         String nh2 = args[3]; //nombre hijo 2
         String nh3 = args[4]; //nombre hijo 3
+        System.out.println("\033[1mFAMILIA: " + apellido + "\033[0m");
         PintarPersona(apellido, nh3);
         PintarPersona(apellido, nh2);
         PintarPersona(apellido, nh1);
         PintarPersona(apellido, np);
     }
 
-    private static void PintarPersona(String apellido, String nombre) {
-        System.out.println("> Padre: " + nombre + " " + apellido);
-        System.out.println("> Primer hijo: " + nombre +" "+ apellido);
-        System.out.println("> Segundo hijo: " + nombre+" "+ apellido);
-        System.out.println("> Tercer hijo: " + nombre +" "+ apellido);
+    protected static void PintarPersona(String apellido, String nombre) {
+        System.out.println( "Nombre: \033[1m " + nombre + " \033[0m " + "Apellido: \033[1m "+ apellido + "\033[0m");
+        
     }
 }
