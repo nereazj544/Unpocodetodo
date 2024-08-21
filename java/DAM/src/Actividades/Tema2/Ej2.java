b344b8afa1d07ec75b236f8787955cca4876e62c
nereazj544 <nzjvirtual@gmail.com>
2024-08-21 17:08:24 +0200

    l

---
+++ java/DAM/src/Actividades/Tema2/Ej2.java
@@ -0,0 +1,10 @@
+package Actividades.Tema2;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        double d = 1234;
+        double d2 = 532;
+        double r = d / d2;
+        System.out.printf("El resultado de la división es: %.15f\n", r);
+    }
+}
