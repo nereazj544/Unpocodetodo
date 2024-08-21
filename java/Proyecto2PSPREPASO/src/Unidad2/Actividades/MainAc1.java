29a28ff56d4e41754f88d427ac99c5af84298715
nereazj544 <nzjvirtual@gmail.com>
2024-08-21 02:52:01 +0200

    .

--- java/Proyecto2PSPREPASO/src/Unidad2/Actividades/MainAc1.java
+++
@@ -1,9 +0,0 @@
-package java.Proyecto2PSPREPASO.src.Unidad2.Actividades;
-
-public class MainAc1 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            new UnHilo(i).start();
-        }
-    }
-}
