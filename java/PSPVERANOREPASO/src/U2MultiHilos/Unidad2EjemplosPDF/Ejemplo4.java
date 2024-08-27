84759e721eeebccd67d6f4718fb4e76e62d467c4
nereazj544 <nzjvirtual@gmail.com>
2024-08-27 14:35:22 +0200

    Unidad 2-Programación MultiHilos

---
+++ java/PSPVERANOREPASO/src/U2MultiHilos/Unidad2EjemplosPDF/Ejemplo4.java
@@ -0,0 +1,24 @@
+package U2MultiHilos.Unidad2EjemplosPDF;
+
+public class Ejemplo4{
+    public static void main(String[] args) throws InterruptedException{
+        Thread hilo = new Thread("cuenta atras"){
+            @Override
+            public void run() {
+                for (int i = 5; i >=0; i--) {
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                System.out.println("hilo finalizacod");
+            }
+
+        };
+        hilo.start();
+        hilo.join();
+        System.out.println("metodo main finalizado");
+        
+    }
+}
