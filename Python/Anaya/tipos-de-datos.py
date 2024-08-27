0a9ce60f1f3a0b577b6d5c782f5ac57f802a27ad
nereazj544 <nzjvirtual@gmail.com>
2024-08-27 17:25:44 +0200

    python (es raro)

---
+++ Python/Anaya/tipos-de-datos.py
@@ -0,0 +1,22 @@
+# TEXTOS
+
+# Texto en una linea
+"string" 'string'
+
+# Texto con más cantidad
+"""Tus datos son:
+    nombre: neo
+    apellido: kurai"""
+'''Tus datos son:
+    nombre: neo
+    apellido: kurai'''
+
+# Numeros
+
+40 # int numero entero
+40.2 # numero float
+
+# Boolean
+
+True 
+False
---
+++ Python/Anaya/variables.py
@@ -0,0 +1,14 @@
+numeros = 40
+
+string = "neo"
+
+numeros += 2
+numeros -= 2
+print(numeros)
+
+# Concatenar
+n= 4
+b = "Hola " + string + " ¿como estas?"
+b3 = f"Hola {n} ¿como estas?" # Convierte numero a texto
+print(b)
+print(b3)
