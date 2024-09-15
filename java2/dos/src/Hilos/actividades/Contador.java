aabd27d1c328adfa34723757f667c6620b4b2faa
nereazj544 <nzjvirtual@gmail.com>
2024-09-16 00:14:17 +0200

    Hilos

--- java2/dos/src/Hilos/actividades/Contador.java
+++ java2/dos/src/Hilos/actividades/Contador.java
@@ -1,5 +1,20 @@
 package Hilos.actividades;
 
+// Pattern \p{Alpha}+
+/*
+ * Contar: lineas, caracteres y palabras de un archvio de txt
+ */
+
+import java.util.regex.Pattern;
+
+
 public class Contador {
-    
+    private Pattern pattern = Pattern.compile("\\p{Alpha}+");
+
+    public static void main(String[] args) {
+         if (args.length < 2) {
+            System.out.println("> Se necesita el modo y el archvio a escanerar.");
+         }
+    }
+
 }
