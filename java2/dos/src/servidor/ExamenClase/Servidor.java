5735c8427b09bed7394e65bed2a1237e139dd78a
nereazj544 <nzjvirtual@gmail.com>
2024-09-15 20:20:24 +0200

    Servidor del examen de clase se necesita un proyecto maven para que carrule

---
+++ java2/dos/src/servidor/ExamenClase/Servidor.java
@@ -0,0 +1,30 @@
+package servidor.ExamenClase;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Servidor {
+	static KeyStore ks;
+	
+	public static void main(String[] args) throws IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
+		ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		ks.load(null, null);//vacio
+		try (ServerSocket serverSocket = new ServerSocket(9000)) {
+			ExecutorService executor = Executors.newFixedThreadPool(100);
+			System.out.println("Servidor ECHO escuchando en puerto 9000");
+			while (true) {
+				Socket socket = serverSocket.accept();
+				executor.submit(new Peticion(socket));
+			}
+		}
+	}
+
+}
